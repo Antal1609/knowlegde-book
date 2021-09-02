0c748e251356390546fe3420ab690182e7f04862
Antal Visser <antalvisser@gmail.com>
2021-09-02 10:36:50 +0200

    Add chat client

---
+++ frontend/src/components/chat/Chat.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="row">
+        <div class="col-md-6 offset-md-3 col-sm-12">
+            <h1 class="text-center">Knowledge Cube</h1>
+            <div>
+                <input type="text" v-model="name" id="username" class="form-control" placeholder="Enter name">
+                <div class="card">
+                    <div class="card-body">
+                        <ul>
+                            <li v-for="(message, key) of messages" :key="key">
+                                {{ message.name }}: {{ message.text }}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <br>
+                <textarea class="form-control" v-model="text"></textarea>
+                <br>
+                <button class="btn" @click.prevent="sendMessage">Send</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { io } from "socket.io-client";
+
+interface Message {
+    name: string
+    text: string
+}
+
+export default defineComponent({
+    name: "Chat",
+    setup() {
+        return {
+            name: "",
+            text: "",
+            messages: ref<Message[]>([]),
+            socket: io('http://172.31.99.6:3000/chat', {
+                transports : ['websocket']
+            })
+        };
+    },
+    created() {
+        this.socket.on("chatToClient", (message) => {
+            this.receivedMessage(message);
+        });
+    },
+    methods: {
+        sendMessage() {
+            if (this.validateInput()) {
+                const message = {
+                    name: this.name,
+                    text: this.text
+                };
+
+                this.messages.push(message);
+                this.socket.emit("chatToServer", message);
+                this.text = "";
+            }
+        },
+        receivedMessage(message: Message) {
+            this.messages.push(message);
+        },
+        validateInput() {
+            return this.name.length > 0 && this.text.length > 0;
+        }
+    }
+});
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/ChatBox.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="chat">
+        <Chat />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Chat from "@/components/chat/Chat.vue";
+
+export default defineComponent({
+    name: "ChatBox",
+    components: {
+        Chat
+    }
+});
+</script>
